611f77f00caf2a9d2766e2ca614584ee2e44620b
ManyaSharma101007 <manyashettysharma@gmail.com>
2021-12-31 11:54:54 +0530

    C-82

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,12 +1,65 @@
 import {React} from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,Image, SafeAreaView,FlatList,StyleSheet,Platform,StatusBar,RFValue, SafeAreaViewBase} from 'react-native';
 
 export default class Feed extends React.Componennt {
     render() {
         return(
-            <View style = {{flex : 1, justifyContent : 'center', alignItems : 'center'}}>
-                <Text> Feed </Text>
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea}/>
+                <View style={styles.appTitle}>
+                    <View style={styles.appIcon}>
+                        <Image
+                            source = {require("../assets/logo.png")}
+                            style={styles.iconImage}
+                        />
+                    </View>
+                    <View style={styles.appTitleTextContainer}>
+                        <Text style={styles.appTitleText}> Spectagram </Text>
+                    </View>
+                </View>
+                <View style={styles.cardContainer}>
+                    <FlatList
+                        keyExtractor = {this.keyExtractor}
+                        data = {posts}
+                        renderItems = {this.renderItems}
+                    />
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        backgroundColor :  "black"
+    },
+    droidSafeArea : {
+        marginTop : Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle : {
+        flex : 0.07,
+        flexDirection : "row",
+    },
+    appIcon : {
+        flex : 0.2,
+        justifyContent : "center",
+        alignItems : "center"
+    },
+    iconImage : {
+        width :  "100%",
+        height : "100%",
+        resizeMode : "contain",
+    },
+    appTitleTextContainer : {
+        flex : 0.8,
+        justifyContent : "center"
+    },
+    appTitleText : {
+        color : "white",
+        fontSize : RFValue(28),
+    },
+    cardContainer : {
+        flex : 0.85 
+    }
+})
---
+++ screens/PostCard.js
@@ -0,0 +1,70 @@
+import {React} from 'react';
+import {View,Text,RFValue,StyleSheet} from 'react-native';
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+export default class PostCard extends React.Componennt {
+    render() {
+        return(
+            <View style={styles.container}>
+                <View style={styles.cardContainer}>
+                    <View style={styles.authorContainer}>
+                        <View style={styles.authorImageContainer}>
+                            <Image
+                                source={require("../assets/profile_img.png")}
+                                style={styles.profileImage}
+                            />
+                        </View>
+                        <View style={styles.authorNameContainer}>
+                            <Text style={styles.authorNameText}> {this.props.post.author} </Text>
+                        </View>
+                    </View>
+                    <Image source={require("../assets/post.jpeg")} styles={styles.postImage}/>
+                    <View style={styles.captionContainer}>
+                        <Text style={styles.captionText}> {this.props.post.caption} </Text>
+                    </View>
+                    <View style={styles.actionContainer}>
+                        <View style={styles.likeButton}>
+                            <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+                            <Text style={styles.likeText}> 12k </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        backgroundColor :  "black"
+    },
+    cardContainer : {
+        flex : 0.85 
+    },
+    profileImage: {
+        width: RFValue(140),
+        height: RFValue(140),
+        borderRadius: RFValue(70)
+    },
+    likeText: {
+        color: "white",
+        fontFamily: "Bubblegum-Sans",
+        fontSize: RFValue(25),
+        marginLeft: RFValue(5)
+    },
+    likeButton: {
+        width: RFValue(160),
+        height: RFValue(40),
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        backgroundColor: "#eb3948",
+        borderRadius: RFValue(30)
+    },
+    actionContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        padding: RFValue(10)
+    },
+})
