5e4f3b6843b49bcd612ef7c9ffcba07d3c3a2c4b
ManyaSharma101007 <manyashettysharma@gmail.com>
2022-01-12 10:58:52 +0530

    SPe

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCdQNzoRdR2iRtNiz9rE5ofeA-UDmY0rXs",
+    authDomain: "spectagram-app-f7db2.firebaseapp.com",
+    projectId: "spectagram-app-f7db2",
+    storageBucket: "spectagram-app-f7db2.appspot.com",
+    messagingSenderId: "157361405518",
+    appId: "1:157361405518:web:0d64d1b8a02a0f7ec58d1f"
+  };
---
+++ screens/LoadingScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {ActivityIndicator,View} from 'react-native';
+
+export default class LoadingScreen extends React.Component {
+
+    componentDidMount(){
+        this.checkIfLoggedIn()
+    }
+
+    checkIfLoggedIn = () => {
+        firebase.auth().onAuthStateChanged((user) =>{
+            if(user){
+                this.props.navigation.navigate("DashboardScreen")
+            }
+            else{
+                this.props.navigation.navigate("LogimScreen")
+            }
+        })
+    }
+
+    render(){
+        return(
+            <View style={{flex : 1, justifyContent : "center", alignItems : "center"}}>
+                <ActivityIndicator size="large" />
+            </View>
+        )
+    }
+}
