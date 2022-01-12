e31604bc6eb51204ee8b05b13c738f90836f6615
ManyaSharma101007 <manyashettysharma@gmail.com>
2022-01-12 11:34:36 +0530

    0

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import {Button,View} from 'react-native';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
+import * as Google from "expo-google-app-auth";
+import firebase from "firebase";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Button } from "react-native-paper";
+
 
 export default class LoginScreen extends React.Component {
-    
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fontsLoaded: false,
+            isEnabled: false,
+            light_theme: true,
+        };
+    }
+
     signInWithGoogleAsync = async () => {
         try {
-            const result = await this.signInWithGoogleAsync.loginAsync({
-                behaviour : "web",
-                iosClientId :
-                "157361405518-6him53pj01b3i3vs3m1m6ueabv6fdda9.apps.googleusercontent.com",
-
-                scopes: ['profile','email'],
-            })
+          const result = await Google.logInAsync({
+            behaviour: "web",
+            androidClientId:
+              "72696421845-lqe44rrjuiggsegp1uv4gklv34tvl3gc.apps.googleusercontent.com",
+            iosClientId:
+            "157361405518-6him53pj01b3i3vs3m1m6ueabv6fdda9.apps.googleusercontent.com",
+            scopes: ["profile", "email"]
+          });
+        } 
+        catch (e) {
+          console.log(e.message);
+          return { error: true };
         }
-    }
+    };
 
     onSignIn = googleUser => {
         // We need to register an Observer on Firebase Auth to make sure auth is initialized.
@@ -79,4 +111,102 @@ export default class LoginScreen extends React.Component {
         }
         return false;
     };
+
+    render(){
+        if(!this.state.fontloaded){
+            return <AppLoading/>
+        }
+        else {
+            return(
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <Image
+              source={require("../assets/logo.png")}
+              style={styles.appIcon}
+            ></Image>
+            <Text style={styles.appTitleText}>{`Spectagram App`}</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.signInWithGoogleAsync()}
+            >
+              <Image
+                source={require("../assets/google_icon.png")}
+                style={styles.googleIcon}
+              ></Image>
+              <Text style={styles.googleText}>Sign in with Google</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.cloudContainer}>
+            <Image
+              source={require("../assets/cloud.png")}
+              style={styles.cloudImage}
+            ></Image>
+          </View>
+        </View>
+            )
+        }
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#15193c"
+    },
+    droidSafeArea: {
+      marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+      flex: 0.4,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    appIcon: {
+      width: RFValue(130),
+      height: RFValue(130),
+      resizeMode: "contain"
+    },
+    appTitleText: {
+      color: "white",
+      textAlign: "center",
+      fontSize: RFValue(40),
+      fontFamily: "Bubblegum-Sans"
+    },
+    buttonContainer: {
+      flex: 0.3,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    button: {
+      width: RFValue(250),
+      height: RFValue(50),
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+      borderRadius: RFValue(30),
+      backgroundColor: "white"
+    },
+    googleIcon: {
+      width: RFValue(30),
+      height: RFValue(30),
+      resizeMode: "contain"
+    },
+    googleText: {
+      color: "black",
+      fontSize: RFValue(20),
+      fontFamily: "Bubblegum-Sans"
+    },
+    cloudContainer: {
+      flex: 0.3
+    },
+    cloudImage: {
+      position: "absolute",
+      width: "100%",
+      resizeMode: "contain",
+      bottom: RFValue(-5)
+    }
+  });
+  
